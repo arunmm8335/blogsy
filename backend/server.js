38e0f220368edd01256316f9b536446897657311
Arun M Myageri <arunmyageri26@gmail.com>
2025-07-13 22:50:30 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ const app = express();
 // CORS configuration
      app.use(cors({
     origin: [
-      'https://blogsy-an11p5vc6-roys-projects-55a11432.vercel.app',
+      'https://blogsy-6tjv8xmxs-roys-projects-55a11432.vercel.app',
       'http://localhost:3000',
       'http://127.0.0.1:3000'
     ],
